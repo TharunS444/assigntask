32a8604a897e25fe3d4c478f8daef0546b8dd36c
tharun <tharun@gmail.com>
2026-03-23 20:36:36 +0530

    added sub.java in master

---
+++ sub.java
@@ -0,0 +1,8 @@
+class sub{
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int difference=a-b;
+        System.out.println("The difference of a and b is: " + difference);
+    }   
+}
