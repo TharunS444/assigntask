4609662df2978d13003eea8c70c30e801eee8431
tharun <tharun@gmail.com>
2026-03-23 20:49:32 +0530

    modified add.java in branch1

--- add.java
+++ add.java
@@ -6,3 +6,4 @@ class add{
         System.out.println("The sum of a and b is: " + sum);
     }
 }
+This code defines a class named `add` with a `main` method that initializes two integer variables `a` and `b` with the values 10 and 20, respectively. It then calculates the sum of `a` and `b`, stores it in the variable `sum`, and prints the result to the console.
